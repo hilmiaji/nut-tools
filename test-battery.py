6a8fe18ebde740f64d1608ae9384052398bebd9d
hilmiaji <92417485+hilmiaji@users.noreply.github.com>
2022-05-30 17:16:48 +0700

    Update test-battery.py

--- test-battery.py
+++ test-battery.py
@@ -3,32 +3,32 @@ import csv
 from subprocess import Popen, PIPE
 import requests
 
-
-delay=60*5    ###for 15 minutes delay
-close_time=time.time()+delay
-tes = subprocess.check_output("upscmd -u sysop -p sysop ups-psn test.battery.start 5", shell=True)
-#print("Send command to UPS : ",tes)
-
-header = ['time','battery_volt']
 array = []
-array_time = []
+
+nama_ups = "ups-psn"
+
+delay=60*5              # Delay 5 menit
+close_time=time.time()+delay
+
+subprocess.check_output(f"upscmd -u sysop -p sysop {nama_ups} test.battery.start 5", shell=True)
+
+
 while close_time > time.time():
 
     date = datetime.datetime.now().strftime("%H:%M:%S")
-    output = subprocess.check_output("upsc ups-psn 2>&1 battery.voltage | grep -v '^Init SSL'", shell=True)
+    output = subprocess.check_output(f"upsc {nama_ups} 2>&1 battery.voltage | grep -v '^Init SSL'", shell=True)
+    output2 = subprocess.check_output(f"upsc {nama_ups} 2>&1 ups.status | grep -v '^Init SSL'", shell=True)
     out = float(output.rstrip())
-    data = ("{},{}".format(date,out))
+    out2 = output2.rstrip()
+    out2 = out2.decode("utf-8")
+    data = ("{},{},{}".format(date,out,out2))
 
     array.append(data)
-    array_time.append(date)
 
-    #requests.get(f"https://api.telegram.org/bot5018630455:AAGtGqco1EKa4gNcyRJ-UKWvOnoGMKgeKHk/sendMessage?chat_id=-723095568&parse_mode=html&text={}")
     time.sleep(2)
-#print(array)
-#print(array_time)
+
 upslokasi = subprocess.check_output("upsc -L", shell=True)
 hasil = "\n".join(array)
-
-sentmessage = "{}\n\n{}".format(upslokasi,hasil)
+sentmessage = "{}\n{}".format(upslokasi.decode("utf-8"),hasil)
 print(hasil)
 requests.get(f"https://api.telegram.org/bot5018630455:AAGtGqco1EKa4gNcyRJ-UKWvOnoGMKgeKHk/sendMessage?chat_id=-769732161&parse_mode=html&text={sentmessage}")
