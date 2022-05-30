fc7f38450bf8adcb7efcecb159face5571264932
hilmiaji <92417485+hilmiaji@users.noreply.github.com>
2022-05-30 16:27:24 +0700

    Update test-battery.py

--- test-battery.py
+++ test-battery.py
@@ -4,9 +4,9 @@ from subprocess import Popen, PIPE
 import requests
 
 
-delay=60    ###for 15 minutes delay
+delay=60*5    ###for 15 minutes delay
 close_time=time.time()+delay
-#tes = subprocess.check_output("upscmd -u admin -p psn ups-psn test.battery.start 1", shell=True)
+tes = subprocess.check_output("upscmd -u sysop -p sysop ups-psn test.battery.start 5", shell=True)
 #print("Send command to UPS : ",tes)
 
 header = ['time','battery_volt']
@@ -23,9 +23,10 @@ while close_time > time.time():
     array_time.append(date)
 
     #requests.get(f"https://api.telegram.org/bot5018630455:AAGtGqco1EKa4gNcyRJ-UKWvOnoGMKgeKHk/sendMessage?chat_id=-723095568&parse_mode=html&text={}")
-    time.sleep(1)
+    time.sleep(2)
 #print(array)
 #print(array_time)
+upslokasi = subprocess.check_output("upsc -L", shell=True)
 hasil = "\n".join(array)
 print(hasil)
-requests.get(f"https://api.telegram.org/bot5018630455:AAGtGqco1EKa4gNcyRJ-UKWvOnoGMKgeKHk/sendMessage?chat_id=-769732161&parse_mode=html&text={hasil}")
+requests.get(f"https://api.telegram.org/bot5018630455:AAGtGqco1EKa4gNcyRJ-UKWvOnoGMKgeKHk/sendMessage?chat_id=-769732161&parse_mode=html&text={upslokasi}\n{hasil}")
