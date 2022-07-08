1474a8b63174ca96b2c5c4139555ef99decfcf39
hilmiaji <92417485+hilmiaji@users.noreply.github.com>
2022-07-08 09:48:45 +0700

    Update staleups.py

--- staleups.py
+++ staleups.py
@@ -4,29 +4,35 @@ from subprocess import Popen, PIPE
 
 print("Initial Setup.....")
 time.sleep(1)
+nama_UPS = "ups"
+
+def restart_ups():
+    try:
+        command1 = 'systemctl stop nut-server'.split()
+        command2 = 'systemctl start nut-server'.split()
+        p1 = Popen(['sudo', '-S'] + command1, stdin=PIPE, stderr=PIPE,
+            universal_newlines=True)
+        sudo_prompt = p1.communicate(sudo_password + '\n')
+
+        time.sleep(5)
+
+        p2 = Popen(['sudo', '-S'] + command2, stdin=PIPE, stderr=PIPE,
+                universal_newlines=True)
+        sudo_prompt = p2.communicate(sudo_password + '\n')
+        print("sukses")
+    except Exception as e:
+        raise e
 
 try :
     sudo_password = 'sysop'
-    theUPS = "ups"
+    
     command = "upsc " + theUPS + " 2>&1 | grep -v '^Init SSL'"
     ups = subprocess.check_output(command, shell=True)
-    staleups = ups.decode('UTF-8')
-    if "stale" in staleups:
-        try:
-            command1 = 'systemctl stop nut-server'.split()
-            command2 = 'systemctl start nut-server'.split()
-            p1 = Popen(['sudo', '-S'] + command1, stdin=PIPE, stderr=PIPE,
-                universal_newlines=True)
-            sudo_prompt = p1.communicate(sudo_password + '\n')
-
-            time.sleep(5)
-
-            p2 = Popen(['sudo', '-S'] + command2, stdin=PIPE, stderr=PIPE,
-                    universal_newlines=True)
-            sudo_prompt = p2.communicate(sudo_password + '\n')
-            print("sukses")
-        except Exception as e:
-            raise e
+    ups_status = ups.decode('UTF-8')
+    if "stale" in ups_status:
+        restart_ups()
+    if "not connected" in ups_status
+        restart_ups()
 
 except Exception as e:
     raise e
